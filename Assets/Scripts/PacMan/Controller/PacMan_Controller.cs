12e6f407375e7fb25206a8f4cd2432b1675dab07
SamFonDom <a23samfondom@inspedralbes.cat>
2026-04-15 17:53:35 +0200

    Rotación dependiendo de la direccion del Input

--- Assets/Scripts/PacMan/Controller/PacMan_Controller.cs
+++ Assets/Scripts/PacMan/Controller/PacMan_Controller.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
-public class PacMan_ : MonoBehaviour
+public class PacMan_Controller : MonoBehaviour
 {
     [SerializeField] float speed;
 
@@ -17,16 +18,39 @@ public class PacMan_ : MonoBehaviour
     {
         moveInput = value.Get<Vector2>();
 
+
         if (moveInput.x != 0 && moveInput.y != 0)
         {
-            Debug.Log("Diagonal");
-
+            moveInput.x = 0;
         }
+
+        RotatePacMan();
         
+    }
+
+    void RotatePacMan()
+    {
+        if (moveInput == Vector2.right)
+        {
+            transform.rotation = Quaternion.Euler(0,0,0);
+        }
+        else if(moveInput == Vector2.left)
+        {
+            transform.rotation = Quaternion.Euler(0,0,180);
+        }
+        else if (moveInput == Vector2.up)
+        {
+            transform.rotation = Quaternion.Euler(0,0,90);
+        }
+        else if (moveInput == Vector2.down)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, -90);
+        }
     }
 
     private void FixedUpdate()
     {
         rb.velocity = moveInput * speed;
+
     }
 }
